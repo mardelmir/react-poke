36618dc8baa30b39519cc64fe408cb9795e7a840
María Delgado <mariad1996@gmail.com>
2024-03-22 16:28:15 +0100

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
 import { useState, useEffect } from 'react'
 import './App.css';
-import { fetchAllPokemons, filterResults, printPokemon } from './utils/apiFunctions';
-
-
+import { fetchAllPokemons, filterResults, fetchResults } from './utils/apiFunctions';
 
 function App() {
   const [search, setSearch] = useState('')
-  const [matchingResults, setMatchingResults] = useState()
+  const [matchingResults, setMatchingResults] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
 
   useEffect(() => {
     const renderPokemon = async () => {
-      setMatchingResults(null), 
-      setLoading(false), 
-      setError('')
-      
-      if (search !== '') {
-        setLoading(true)
-        const results = await fetchAllPokemons()
-        const filteredResults = filterResults(results, search)
-
-        // FIXME: Hace la búsqueda con el state ANTERIOR (mirar documentación de React para meter cleanup code) 
-        results.length === 0 || filteredResults.length == 0
-          ? (setError('Pokémon no encontrado'))
-          : (setError(''), setMatchingResults([...filteredResults]))
-
-        console.log(matchingResults)
-        console.log(error)
-      } else { setError('') }
-    }
-    renderPokemon()
-    return () => { setMatchingResults(null), setLoading(false), setError('') }
-
-  }, [search])
-
-
+      setLoading(true); 
+  
+      try {
+        if (search !== '') {
+          const results = await fetchAllPokemons(search);
+          const filtered = filterResults(results, search);
+          const matchingPokemons = await fetchResults(filtered);
+  
+          if (matchingPokemons.length === 0) {
+            setError('Pokémon no encontrado');
+            setMatchingResults([]);
+          } else {
+            setError('');
+            setMatchingResults(matchingPokemons);
+          }
+        } else {
+          setError('');
+          setMatchingResults([]);
+        }
+      } catch (error) {
+        setError('Error al cargar los Pokémons: ' + error);
+        setMatchingResults([]);
+      }
+  
+      setLoading(false); 
+    };
+  
+    renderPokemon();
+  }, [search]); 
+  
   return (
     <>
       <form>
@@ -43,10 +47,18 @@ function App() {
         <input type='text' name='search' value={search} onChange={(e) => { setSearch((e.target.value).toLowerCase().trim()) }} />
       </form>
       <div className='loading'>{loading}</div>
-      {/* <div className='result'>{result}</div> */}
+      <div className='matchingResults'>{matchingResults && (
+        matchingResults.map((pokemon, i) =>
+          <div key={i}>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <p>Nombre: {pokemon.name}</p>
+            <p>Tipo: {pokemon.type.join(', ')}</p>
+          </div>)
+      )}</div>
       <div className='error'>{error}</div>
     </>
   )
 };
 
 export default App;
+
--- src/utils/apiFunctions.jsx
+++ src/utils/apiFunctions.jsx
@@ -1,22 +1,38 @@
-const fetchAllPokemons = async () => {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1400`)
-        if (!response.ok) { throw new Error('Response.ok = false') }
-        const data = await response.json()
-        return data.results
+const fetchAllPokemons = async (search) => {
+    if (search !== '')
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1400`)
+            if (!response.ok) { throw new Error('Error: la solicitud para todos los pokémon no ha sido exitosa') }
+            const data = await response.json()
+            return data.results
+        }
+        catch (error) { console.log(error) }
+}
+
+const filterResults = (results, search) => {
+    const match = results.filter(pokemon => pokemon.name.startsWith(search))
+    return match.map(pokemon => pokemon.url)
+}
+
+const fetchResults = async (matchingPokemons) => {
+    if (matchingPokemons.length !== 0) {
+        const matchInfo = []
+        for (let url of matchingPokemons) {
+            try {
+                const response = await fetch(url)
+                if (!response.ok) { throw new Error('Error: la solicitud para las coincidencias no ha sido exitosa') }
+                const data = await response.json()
+                const info = {
+                    name: data.name,
+                    image: data.sprites.front_default,
+                    type: data.types.map(e => e.type.name)
+                }
+                matchInfo.push(info)
+            }
+            catch (error) { console.log(error) }
+        }
+        return matchInfo
     }
-    catch (error) { console.log(error) }
 }
 
-const filterResults = (results, search) => results.filter(pokemon => pokemon.name.startsWith(search))
-
-const printPokemon = (result) => {
-    result.forEach(pokemon => {
-        <div>
-            <img src={pokemon.image} alt={pokemon.name} />
-            <p>Nombre: {pokemon.name}</p>
-        </div>
-    })
-}
-
-export { fetchAllPokemons, filterResults, printPokemon }
+export { fetchAllPokemons, filterResults, fetchResults }
